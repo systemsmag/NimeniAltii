5ced4f6140bf3b13225a4f36cc238e83f5401afe
SystemS <systemsmag1992@gmail.com>
2021-04-20 07:02:07 +0300

    End of section 6

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosResponse } from 'axios';
+import { IActivity } from '../models/activity';
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+const responeBody = (respone: AxiosResponse) => respone.data;
+
+const sleep = (ms: number) => (response: AxiosResponse) =>
+    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+
+const requests = {
+    get: (url: string) => axios.get(url).then(sleep(1000)).then(responeBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responeBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responeBody),
+    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responeBody)
+}
+
+const Activities = {
+    list: (): Promise<IActivity[]> => requests.get('/activities'),
+    details: (id: string) => requests.get(`/activities/${id}`),
+    create: (activity: IActivity) => requests.post('/activities', activity),
+    update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del(`/activities/${id}`)
+}
+
+const sActivities = {
+    Activities
+}
+
+export default sActivities;
